1e00794727f6a480798731ee5648bbdb95cab229
techasit <techasit2424@gmail.com>
2026-07-10 02:45:58 +0700

    sssssssssssssssssssey

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,9 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
-			build: {
+  		})
+
+	],	build: {
     	sourcemap: false
-  		}
-		})
-	]
+		}
 });
