60d0debafbb51f6741a747d10e47b4ffad5d0a05
techasit_laptop <techasit2424@gmail.com>
2026-07-10 09:32:36 +0700

    sssss

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
-  		})
-
-	],	build: {
-    	sourcemap: false
-		}
+    plugins: [
+        tailwindcss(),
+        sveltekit({
+            compilerOptions: {
+                runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+            }
+        })
+    ],
+    // ย้าย bodySizeLimit มาไว้ในส่วน config ของ adapter-node ตรงนี้
+    adapter: adapter({
+        bodySizeLimit: 100 * 1024 * 1024 // 100MB
+    }),
+    // บังคับปิด sourcemap สำหรับฝั่ง client และ server ตอน build
+    build: {
+        sourcemap: false
+    }
 });
